a53566a8bd55f9529ffa86288e28030033a608cd
Inf1n1ty75 <volkandu75@gmail.com>
2024-05-08 13:53:00 +0200

    correct loading bar

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,12 +6,11 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 17:23:33 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/05/08 12:26:46 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/05/08 13:52:01 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
-#include <pthread.h>
 
 int	main(int ac, char **av)
 {

198e3bb41659d998578add43287b1c976a5395ae
Inf1n1Ty_75 <volkandu75@gmail.com>
2024-05-16 01:51:36 +0200

    ok not bad, my philo are starving when it s very too close. prob because usleep can sleep more than required ? ex 20 400 200 200, they can live 1/10 case

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,11 +6,17 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 13:10:48 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/05/15 17:34:50 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/05/16 01:49:09 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+static	void	sleep_if_many_philo(t_philo *philo)
+{
+	if (philo->philo_nb < 31)
+		ft_usleep(1, philo->program, philo);
+}
 
 static	void	*one_philo(t_philo *philo)
 {
@@ -32,10 +38,10 @@ static void	lock_unlock_fork(enum e_fork fork, t_philo *philo)
 		}
 		else
 		{	
-			ft_usleep(1, philo->program, philo);
+			sleep_if_many_philo(philo);
 			pthread_mutex_lock(philo->l_fork);
 			print_message(TAKEN_A_FORK, philo);
-			ft_usleep(1, philo->program, philo);
+			sleep_if_many_philo(philo);
 			pthread_mutex_lock(philo->r_fork);
 			print_message(TAKEN_A_FORK, philo);
 		}
@@ -65,8 +71,6 @@ void	*routine(t_philo *philo)
 {
 	if (philo->philo_nb == 1)
 		return (one_philo(philo));
-	if (philo->philo_id % 2 != 0)
-		ft_usleep(1, philo->program, philo);
 	while (1)
 	{
 		pthread_mutex_lock(&philo->program->death_lock);
@@ -78,7 +82,6 @@ void	*routine(t_philo *philo)
 		}
 		pthread_mutex_unlock(&philo->program->death_lock);
 		eat_and_sleep(philo);
-		ft_usleep(1, philo->program, philo);
 		print_message(THINKING, philo);
 	}
 	return (NULL);
